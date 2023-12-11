92a7de75992e7bcc3fba78c73d649cf52e8ff808
Pedro Augusto <pedromartins036@gmail.com>
2023-12-11 20:58:42 -0300

    Create Book Constructor

--- src/main/java/com/library/domain/Book.java
+++ src/main/java/com/library/domain/Book.java
@@ -1,27 +1,34 @@
 package com.library.domain;
 
 
+import com.library.dtos.BookDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity(name = "books")
-@Table(name = "books")
+@Table(name = "books", uniqueConstraints = @UniqueConstraint(columnNames = {"name", "author"}))
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     private String author;
     private String description;
-    private long stock;
+    private Long stock;
     private BigDecimal price;
+
+    public Book(BookDTO data) {
+        this.name = data.name();
+        this.author = data.author();
+        this.description = data.description();
+        this.stock = data.stock();
+        this.price = data.price();
+    }
 }
