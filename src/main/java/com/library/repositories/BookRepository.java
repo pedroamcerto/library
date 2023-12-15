0138e9cf776cf4594d52299d46aeded92a1d9a67
Pedro Augusto <pedromartins036@gmail.com>
2023-12-14 22:26:38 -0300

    Updated BookRepository

--- src/main/java/com/library/repositories/BookRepository.java
+++ src/main/java/com/library/repositories/BookRepository.java
@@ -1,12 +1,7 @@
 package com.library.repositories;
 
-import com.library.domain.Book;
+import com.library.domain.Book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface BookRepository extends JpaRepository<Book, Long> {
-    Optional<Book> findBookByName(String name);
-    Optional<Book> findBookByAuthor(String author);
-    Optional<Book> findBookById(Long id);
 }
