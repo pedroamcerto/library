6e4d9bafd6f8c851da5df216fd464ab620ffb23b
Pedro Augusto <pedromartins036@gmail.com>
2023-12-14 22:25:16 -0300

    Revert "Update BookController"
    This reverts commit e3771fe09261c4e43c8bfa3fa55ed57c57f0c2c1.

--- src/main/java/com/library/controllers/BookController.java
+++ src/main/java/com/library/controllers/BookController.java
@@ -1,8 +1,8 @@
 package com.library.controllers;
 
-import com.library.domain.Book.Book;
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.library.domain.Book;
 import com.library.dtos.BookDTO;
-import com.library.dtos.ResponseDTO;
 import com.library.services.BookService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.ResourceBundle;
 
 @RestController()
 @RequestMapping("/books")
@@ -19,25 +18,17 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+
     @PostMapping
-    public ResponseEntity<ResponseDTO> createBook(@RequestBody BookDTO book){
+    public ResponseEntity<Book> createBook(@RequestBody BookDTO book){
         Book newBook = bookService.createBook(book);
-        return new ResponseEntity<>(new ResponseDTO("Livro criado com sucesso!", 201), HttpStatus.CREATED);
+        return new ResponseEntity<>(newBook, HttpStatus.CREATED);
     }
+
     @GetMapping
     public ResponseEntity<List<Book>> getAllBooks(){
         List<Book> books = this.bookService.getAllBooks();
         return new ResponseEntity<>(books, HttpStatus.OK);
-    }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ResponseDTO> deleteBook(@PathVariable Long id){
-        this.bookService.deleteBook(id);
-        return new ResponseEntity<>(new ResponseDTO("Livro deletado com sucesso.", 200), HttpStatus.OK);
-    }
-    @PutMapping("/{id}")
-    public ResponseEntity<ResponseDTO> updateBook(@PathVariable Long id, @RequestBody BookDTO book) throws Exception {
-        Book updatedBook = this.bookService.updateBook(id, book);
-        return new ResponseEntity<>(new ResponseDTO("Livro atualizado com sucesso", 200), HttpStatus.OK);
 
     }
 }
