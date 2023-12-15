1b2371b04ac6252239cc6b7391d96a905eaa24c3
Pedro Augusto <pedromartins036@gmail.com>
2023-12-14 22:26:11 -0300

    Update BookDTO
    Created validations in this DTO

--- src/main/java/com/library/dtos/BookDTO.java
+++ src/main/java/com/library/dtos/BookDTO.java
@@ -1,14 +1,23 @@
 package com.library.dtos;
 
+import jakarta.validation.constraints.*;
+
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
-public record BookDTO(
-        String name,
-        String author,
-        String description,
-        Long stock,
-        BigDecimal price) {
-
-
-
+@Data
+public class BookDTO {
+    @NotBlank private String name;
+    @NotBlank private String author;
+    @NotBlank private String description;
+    @Positive private Long stock;
+    @DecimalMin(value = "0") private BigDecimal price;
 }
+
+
+
